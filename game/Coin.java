5305dbbf7eb465a0d045e7eafeb7b02ffdc019ca
hongjunpyo <0721hjp@naver.com>
2024-06-15 16:15:33 +0900

    feat: Coin구매기능 구현완료

--- game/Coin.java
+++ game/Coin.java
@@ -1,5 +1,6 @@
 package com.project.phone.game;
 
+import com.project.phone.bank.CreateAccount;
 import com.project.phone.bank.Money;
 import com.project.phone.database.Connecting;
 import com.project.phone.util.Tools;
@@ -10,6 +11,7 @@ public class Coin {
 
     Scanner in = new Scanner(System.in);
     Connecting connecting = Connecting.getInstance(); //db연결 객체 생성
+    CreateAccount createAccount = CreateAccount.getInstance(connecting);
     private static Coin coin;
     private int gameCoin;
 
@@ -58,17 +60,30 @@ public class Coin {
         if (input.equals("home")) {
             return 0;
         } else if (input.equals("코인 구매하기") || input.equals("1")) {
-            System.out.println("현 통장 잔고는" + Money.getCash() + "입니다");
-            System.out.println("10원에 코인 1개 입니다. 코인를 몇개 구입하시겠습니까?");
+            System.out.printf("현 통장 잔고는 %,d(won) 입니다.\n", Money.getCash());
+            System.out.println("10원에 코인 1개 입니다. 몇개 구입하시겠습니까?");
+            System.out.print("=>:");
             intput = this.in.nextInt();
-            System.out.println("총 : " + intput*10 + " 원 입니다. 구매하시겠습니까?");
-            System.out.println("yes/no");
+            System.out.print("총" + intput*10 + " 원 입니다. 구매하시겠습니까?(yes/no):");
             input = this.in.next();
             if (input.equals("yes")) {
-                System.out.println("구매완료되었습니다");
-                Tools.pause(1);
-                this.gameCoin += intput;
-                System.out.println("남은 계좌 잔액:" + Money.getCash() + "/ 코인:" + this.gameCoin);
+
+                if(intput*10 > Money.getCash()){
+                    System.out.println("잔액이 부족합니다.");
+                    Tools.pause(1);
+                    return 0;
+                }else{
+                    Money.subMoney(createAccount.getAccountNumber(), intput*10, "Coin");
+                    System.out.println("구매완료되었습니다");
+                    Tools.pause(1);
+                    this.gameCoin += intput;
+                    System.out.println("남은 계좌 잔액:" + Money.getCash() + "/ 코인:" + this.gameCoin);
+                    Tools.pause(1);
+                    return 0;
+                }
+            }
+            else{
+                System.out.println("취소합니다.");
                 Tools.pause(1);
                 return 0;
             }
