6d0ccaa9e20e3b45cec48da1a1508523df18ec1e
june <0721hjp@naver.com>
2026-09-17 09:49:54 +0900

    init

--- src/com/project/phone/database/DBsetting.java
+++ src/com/project/phone/database/DBsetting.java
@@ -4,5 +4,5 @@ public class DBsetting {
     private DBsetting(){ }
     public static final String url = "jdbc:mysql://localhost:3306/phone";
     public static final String username = "root";
-    public static final String password = "";
+    public static final String password = "1234";
 }
